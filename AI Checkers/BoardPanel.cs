1ab8933be20660de731a8fb7c50358bbe1fee8cf
toome <toome@LAPTOP-S683NICJ>
2019-05-30 17:21:43 +0200

    backup3

--- AI Checkers/BoardPanel.cs
+++ AI Checkers/BoardPanel.cs
@@ -9,38 +9,43 @@ using AICheckers.Properties;
 
 namespace AICheckers
 {
-    public class BoardPanel : Panel
+    class BoardPanel : Panel
     {
         IAI AI = null;
 
-        //Assets
+        //Zainicjalizowanie kwadracików kolorami
+        Color darkSquare = Color.DarkGray;
+        Color lightSquare = Color.Gainsboro;
+
+        //Upload obrazków 
         Image checkerRed = Resources.checkerred;
         Image checkerRedKing = Resources.checkerredking;
         Image checkerBlack = Resources.checkerblack;
         Image checkerBlackKing = Resources.checkerblackking;
 
-        Color darkSquare = Color.White;
-        Color lightSquare = Color.Gainsboro;
-
-        bool animating = false;
+        //Animacje
+        private bool animating = false;
         const int animDuration = 1000;
-        Square animPiece;
-        Point oldPoint = new Point(-1, -1);
-        Point currentPoint = new Point(-1, -1);
-        Point newPoint = new Point(-1, -1);
-        int delta = 10;
+        private Square animPiece;
+
+        //Ustawienie domyślnych wartości dla punktów
+        private Point oldPoint = new Point(-1, -1);
+        private Point currentPoint = new Point(-1, -1);
+        private Point newPoint = new Point(-1, -1);
 
         int squareWidth = 0;
 
-        Point selectedChecker = new Point(-1, -1);
-        List<Move> possibleMoves = new List<Move>();
-        //List<Point> highlightedSquares = new List<Point>();
+        //Pola do operowania warcabami
+        private Point selectedChecker = new Point(-1, -1);
+        private List<Move> possibleMoves = new List<Move>();
 
+        //Określenie tury 
         CheckerColour currentTurn = CheckerColour.Black;
         private System.ComponentModel.IContainer components;
 
-        Square[,] Board = new Square[8,8];
+        private Square[,] Board = new Square[8,8];
         
+        //Inicjalizacja panelu
         public BoardPanel()
             : base()
         {
@@ -48,7 +53,7 @@ namespace AICheckers
             this.ResizeRedraw = true;
             this.SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.UserPaint | ControlStyles.Opaque | ControlStyles.AllPaintingInWmPaint, true);
 
-            //Initialize board
+            //Inicjalizacja planszy
             for (int i = 0; i < 8; i++)
             {
                 for (int j = 0; j < 8; j++)
@@ -57,8 +62,8 @@ namespace AICheckers
                     Board[i, j].Colour = CheckerColour.Empty;
                 }
             }
-
-            //Setup Pieces
+            
+            //Przygotowanie warcabów (rozmieszczenie na planszy czarnych i czerwonych)
             for (int i = 0; i < 8; i += 1)
             {
                 int offset = 0;
@@ -81,13 +86,12 @@ namespace AICheckers
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            //Logic
 
-            //Draw
+            //Rysowanie kwadracików
             base.OnPaint(e);                        
             e.Graphics.Clear(lightSquare);
 
-            //Draw the board
+            //Rysowanie planszy
             squareWidth = (Width) / 8;
             for (int c = 0; c < Width; c += squareWidth)
             {
@@ -102,30 +106,30 @@ namespace AICheckers
                 }
             }
 
-            //Draw possible moves
+            //Pokazanie możliwych ruchów
             foreach (Move move in possibleMoves)
             {
                 e.Graphics.FillRectangle(Brushes.PaleTurquoise, move.Destination.X * squareWidth, move.Destination.Y * squareWidth, squareWidth, squareWidth);
             }
 
-            //Draw selected checker
+            //Po wybraniu warcaba
             if (selectedChecker.X >= 0 && selectedChecker.Y >= 0)
             {
                 e.Graphics.FillRectangle(Brushes.PeachPuff, selectedChecker.X * squareWidth, selectedChecker.Y * squareWidth, squareWidth, squareWidth);
             }
 
-            //Draw Border
+            //Narysowanie granicy
             e.Graphics.DrawRectangle(Pens.DarkGray,
             e.ClipRectangle.Left,
             e.ClipRectangle.Top,
             e.ClipRectangle.Width - 1,
             e.ClipRectangle.Height - 1);
 
-            //Set for higher quality resizing of images
-            //e.Graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            //W celu fajniejszego konwertowania obrazków
+            e.Graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
             e.Graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
 
-            //Draw Checker Images
+            //Pobranie obrazków w celu przedstawienia ich na planszy
             for (int i = 0; i < 8; i++)
             {
                 for (int j = 0; j < 8; j++)
@@ -154,21 +158,12 @@ namespace AICheckers
                     }
                 }
             }
-
-            //if (animating)
-            //{
-            //    Console.WriteLine("Animating" + currentPoint.ToString());
-            //    //currentPoint.X += -1 * (newPoint.X - oldPoint.X) / 10;
-            //    //currentPoint.Y += -1 * (newPoint.Y - oldPoint.Y) / 10;
-
-            //    e.Graphics.DrawImage(checkerBlack, new Rectangle(currentPoint.Y, currentPoint.Y, squareWidth, squareWidth));
-
-            //    if (currentPoint.X == newPoint.X && currentPoint.Y == newPoint.Y)
-            //        animating = false;
-            //}
-
         }
         
+        /// <summary>
+        /// Metoda wykonująca się po kliknięciu myszy
+        /// </summary>
+        /// <param name="e"></param>
         protected override void OnMouseClick(MouseEventArgs e)
         {
             int clickedX = (int)(((double)e.X / (double)Width) * 8.0d);
@@ -176,27 +171,25 @@ namespace AICheckers
 
             Point clickedPoint = new Point(clickedX, clickedY);
 
-            //Determine if this is the correct player
+            
             if (Board[clickedY, clickedX].Colour != CheckerColour.Empty
                 && Board[clickedY, clickedX].Colour != currentTurn)
                 return;
           
-            //Determine if this is a move or checker selection
+            //Sprawdzamy, czy gracz się porusza czy tylko wybiera warcaba
             List<Move> matches = possibleMoves.Where(m => m.Destination == clickedPoint).ToList<Move>();
             if (matches.Count > 0)
             {
-                //Move the checker to the clicked square
                 MoveChecker(matches[0]);
             }
             else if (Board[clickedY, clickedX].Colour != CheckerColour.Empty)
             {
-                //Select the clicked checker
+                //Po kliknięciu w warcaba wchodzimy w tryb wybierania i pokazujemy możliwe ruchy
                 selectedChecker.X = clickedX;
                 selectedChecker.Y = clickedY;
                 possibleMoves.Clear();
-
-                Console.WriteLine("Selected Checker: {0}",selectedChecker.ToString());
                 
+                //Pokazujemy nową planszę po ruchu
                 Move[] OpenSquares = Utils.GetOpenSquares(Board, selectedChecker);
                 possibleMoves.AddRange(OpenSquares);
 
@@ -204,30 +197,36 @@ namespace AICheckers
             }            
         }
 
+        /// <summary>
+        /// Metoda odpowiedzialna za poruszanie warcabem
+        /// Jako argument przyjmuje ruch, jaki wykonał gracz
+        /// </summary>
+        /// <param name="move"></param>
         private void MoveChecker(Move move)
         {
-            Console.WriteLine(move.ToString());
-
             Board[move.Destination.Y, move.Destination.X].Colour = Board[move.Source.Y, move.Source.X].Colour;
             Board[move.Destination.Y, move.Destination.X].King = Board[move.Source.Y, move.Source.X].King;
             ResetSquare(move.Source);
 
+            // Odświeżamy kwadraciki
             foreach (Point point in move.Captures)
                 ResetSquare(point);
 
+            //"Odznaczenie" warcaba
             selectedChecker.X = -1;
             selectedChecker.Y = -1;
 
-            //Kinging
+            //Zamiana warcaba w króla
             if ((move.Destination.Y == 7 && Board[move.Destination.Y, move.Destination.X].Colour == CheckerColour.Red)
                 || (move.Destination.Y == 0 && Board[move.Destination.Y, move.Destination.X].Colour == CheckerColour.Black))
             {
                 Board[move.Destination.Y, move.Destination.X].King = true;
             }
 
-
+            //Czyścimy bufor z możliwymi ruchami, aby nie pokazywały się przy kolejnej turze stare możliwości
             possibleMoves.Clear();
 
+            // Ruch
             oldPoint.X = move.Source.Y * squareWidth;
             oldPoint.Y = move.Source.X * squareWidth;
             newPoint.X = move.Destination.Y * squareWidth;
@@ -237,9 +236,14 @@ namespace AICheckers
 
             this.Invalidate();
 
+            //Zmiana tury
             AdvanceTurn();
         }
 
+        /// <summary>
+        /// Czyszczenie kwadracików
+        /// </summary>
+        /// <param name="square"></param>
         private void ResetSquare(Point square)
         {
             //Reset the square and the selected checker
@@ -247,6 +251,9 @@ namespace AICheckers
             Board[square.Y, square.X].King = false;
         }
 
+        /// <summary>
+        /// Metoda rozstrzygająca kto rozgrywa turę
+        /// </summary>
         private void AdvanceTurn()
         {
             if (currentTurn == CheckerColour.Red)
@@ -261,6 +268,7 @@ namespace AICheckers
             if (AI != null && AI.Colour == currentTurn)
             {
                 Move aiMove = AI.Process(Board);
+                //TUTAJ RUSZA SIĘ KOMPUTER
                 MoveChecker(aiMove);
             }
         }
